c1b99ac7e0824d077aaaa7b5610b30a5c82692be
SojinPlusPark <sojinpark.seoul@gmail.com>
2023-09-08 13:25:55 +0200

    connected App.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 x//import the react, react DOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App';
 //get a reference to the div(ELEMENT) with ID root
 const el = document.getElementById('root');
 //tell React to take control of that ELEMENT
 const root = ReactDOM.createRoot(el);
 //create a COMPONENT (returns JSX)
-function App() {
-    let message = 'bye there!'
-    if (Math.random() > 0.5) {
-        message = 'hi there!'
-    }
-    return <h1>{message}This is an web application of the Zero Waste Gourmet</h1>;
-}
+
 //show that COMPONENT on the screen
 root.render(<App />);

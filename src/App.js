bae4eb65947ad0b7eca4157bc23b3bae9c17c825
SojinPlusPark <sojinpark.seoul@gmail.com>
2023-09-30 17:49:32 +0200

    update json files, app.js, index.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,61 @@
+import 'bulma/css/bulma.css';
+import './App.css';
+import SearchBar from './SearchBar';
+import Navbar from './Navbar'
+import Footer from './Footer';
+import Recipe from './Recipe';
+
+
 function App() {
-    return <h1>Hi! This is an web application of the Zero Waste Gourmet</h1>
+    return (
+        <div>
+
+            <Navbar />
+            <SearchBar />
+
+            <div className='container'>
+                <section className='section'>
+                    <div className='columns'>
+                        <div className='column is-4'>
+                            <Recipe
+                                title='Veggie Stock'
+                                // image={ }
+                                description='You can use vegetable scraps to make stock'
+                            />
+                        </div>
+                        <div className='column is-4'>
+                            <Recipe
+                                title='Mushroom patties'
+                                description='You can use end of mushrooms as well. Yummy!'
+                            />
+                        </div>
+                        <div className='column is-4'>
+                            <Recipe
+                                title='Cauliflower soup'
+                                description='You can use cauliflower stems as well, with other ingredients.'
+                            />
+                        </div>
+                        <div className='column is-4'>
+                            <Recipe
+                                title='Broccoli soup'
+                                description='You can use broccoli stalks. It will be more delicious and healthy!'
+                            />
+                        </div>
+                    </div>
+                </section>
+            </div>
+
+            <Footer />
+
+        </div>
+    );
 }
+
 export default App;
+
+// <section className="hero">
+//                 <div className="hero-body">
+//                     <p className="title">ZERO WASTE GOURMET</p>
+//                     <p className="subtitle">Cook & Make No Waste With Our Recipes </p>
+//                 </div>
+//             </section>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
-x//import the react, react DOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-//get a reference to the div(ELEMENT) with ID root
+
 const el = document.getElementById('root');
-//tell React to take control of that ELEMENT
 const root = ReactDOM.createRoot(el);
 //create a COMPONENT (returns JSX)
 
-//show that COMPONENT on the screen
 root.render(<App />);

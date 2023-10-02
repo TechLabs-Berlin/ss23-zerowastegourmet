d0698bd6a06fd321e9acc483151f4eb5e8f277f9
SojinPlusPark <sojinpark.seoul@gmail.com>
2023-10-02 16:47:56 +0200

    update

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,73 +1,70 @@
+import React, { useState } from "react";
 import "./Navbar.css";
 import logo_temp from "./images/logo_temp.png";
-import iconHeart from "./images/Heart.svg";
+import iconHeart from "./images/heart.svg";
 import iconKitchen from "./images/kitchen.svg";
 import iconCalendar from "./images/calendar.svg";
-import iconSearch from "./images/search.svg";
 
 function Navbar() {
+    const [isActive, setIsActive] = useState(false);
+
+    const toggleMenu = () => {
+        setIsActive(!isActive);
+    }
+
     return (
         <nav className="navbar" role="navigation" aria-label="main navigation">
-            {/* <div className="navbar-brand"> */}
-            <a className="navbar-item" href="/">
-                <img src="./images/logo_temp.png" alt="logo_zerowastegourmet" width="112" />
-            </a>
+            <div className="navbar-brand">
+                <a className="navbar-item" href="/">
+                    <img src="https://cdn.animaapp.com/projects/6519371fc7f7d75d0d661f4d/releases/6519381acdfbbe8c78008698/img/---icon--branch-1--1@2x.png" alt="logo_zerowastegourmet" />
 
-            <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasic">
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-            </a>
-            {/* </div> */}
+                    {/* <img src={logo_temp} alt="logo_zerowastegourmet" width="150" /> */}
+                </a>
 
-            <div className="navbar-menu" id="navbarBasic" >
+                <a
+                    role="button"
+                    className={`navbar-burger ${isActive ? "is-active" : ""}`}
+                    aria-label="menu"
+                    aria-expanded="false"
+                    onClick={toggleMenu}
+                // data-target="navbarBasic"
+                >
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                </a>
+            </div>
+
+            <div className={`navbar-menu ${isActive ? "is-active" : ""}`}>
+                {/* <div className="navbar-start"> */}
                 <a className="navbar-item">
-                    <img className="navbar-icon-heart" src={iconHeart} />
+                    <img className="navbar-icon-heart" src={iconHeart} alt="Heart" />
                     Favorite
                 </a>
 
                 <a className="navbar-item">
-                    <img className="navbar-icon" src={iconKitchen} />
+                    <img className="navbar-icon" src={iconKitchen} alt="Kitchen" />
                     My Kitchen
                 </a>
 
                 <a className="navbar-item">
-                    <img className="navbar-icon" src={iconCalendar} />
+                    <img className="navbar-icon" src={iconCalendar} alt="Calendar" />
                     Weekly Plan
                 </a>
+            </div>
 
-                <div className="navbar-item">
-                    <div className="buttons">
-                        <a className="button">
-                            Sign up
-                        </a>
-                        <a className="button">
-                            Log in
-                        </a>
-                    </div>
+            {/* <div className="navbar-end"> */}
+            <div className="navbar-item">
+                <div className="buttons">
+                    <a className="button">Sign up</a>
+                    <a className="button">Log in</a>
                 </div>
             </div>
-        </nav>
+            {/* </div> */}
+            {/* </div> */}
+        </nav >
     );
 }
 
+
 export default Navbar;
-
-
-// is-fixed-top
-
-
-
-
-//     <nav className="navbar">
-//     <a href="/" className="logo">
-//         <img src="logo_temp" alt="logo" />
-//     </a>
-//     <ul className="nav-links">
-//         <li className="nav-item"><a href="#">Favorite</a></li>
-//         <li className="nav-item"><a href="#">My Kitchen</a></li>
-//         <li className="nav-item"><a href="#">Weekly Plan</a></li>
-//         <li className="nav-item"><a href="#">Sign in</a></li>
-//         <li className="nav-item"><a href="#">Log in</a></li>
-//     </ul>
-// </nav >

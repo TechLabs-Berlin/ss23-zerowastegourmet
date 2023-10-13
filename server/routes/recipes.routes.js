d599def02f63d34d1610071e245752b1b1a038ad
Guilherme Mazzolini <guilhermemazzolini@hotmail.com>
2023-10-13 17:10:03 +0200

    connection working

--- server/routes/recipes.routes.js
+++ server/routes/recipes.routes.js
@@ -12,12 +12,12 @@ router.post('/recipes', async (req, res) => {
       console.log('Received data:', data);
 
       // Send the received data to the Flask API
-      const flaskURL = "http://127.0.0.1:1000/api/ingredient_categories"; // Update the URL
-      const response = await axios.post(flaskURL, { data }); // Send the data to the Flask API
+      const flaskURL = "http://127.0.0.1:1000/api/filter_recipes"; // Update the URL
+      const response = await axios.post(flaskURL, { ingredients: data }); // Send the data to the Flask API
 
-      console.log('Received response from Flask:', response.data.message);
+      console.log('Received response from Flask:', response.data);
 
-      res.status(200).json({ message: 'Data received and stored successfully' });
+      res.status(200).json({ recipes: response.data });
   } catch (error) {
       console.error('Error:', error);
       res.status(500).json({ error: 'Internal server error' });
